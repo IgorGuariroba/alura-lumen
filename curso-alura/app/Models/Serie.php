250de0c76f665d20f74cb70a00ae88df28a72cdd
igor.guariroba <igor.guariroba@jsl.com.br>
2021-11-26 21:28:06 -0300

    Implementção do metodo update serie

--- curso-alura/app/Models/Serie.php
+++ curso-alura/app/Models/Serie.php
@@ -2,13 +2,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Serie extends Model
 {
     public $timestamps = false;
     protected $fillable = ['nome'];
 
-    public function episodios()
+    /** @noinspection PhpUnused */
+    public function episodios(): HasMany
     {
         return $this->hasMany(Episodio::class);
     }
